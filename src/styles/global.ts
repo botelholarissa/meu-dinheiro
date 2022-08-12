aa0d447f350f15eb6a389a977979d36df64c8a53
botelholarissa <conta.larissa@gmail.com>
2022-08-12 17:47:33 -0300

    add global styles

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -1,3 +1,27 @@
 import { createGlobalStyle } from "styled-components";
 
-export const Container = createGlobalStyle``;
+export const Container = createGlobalStyle`
+
+    * {
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+    }
+
+    :root {
+        --green-green: #33CC95;
+        --text-green: #29A376;
+        --background-gray: #F0F2F5;
+        --text-gray: #969CB2;
+        --title: #363F5F;
+        --walter-white: #FFFFFF;
+        --red-red: #E52E4D;
+        --blue-blue: #0000FF;
+        --logo-blue: #57DBCA;
+        --transaction-orange:#E06E00;
+    }
+
+
+
+`;
+
