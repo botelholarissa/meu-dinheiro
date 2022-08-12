a703e49cc94ba9c1cc36dfab6db4f942dd47d2b3
botelholarissa <conta.larissa@gmail.com>
2022-08-12 19:17:13 -0300

    add styles to header

--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -1,5 +1,29 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
-    background: red;
+   background: var(--blue-blue);
+`
+
+export const HeaderContent = styled.header`
+    max-width: 1120px;
+    margin: 0 auto;
+    display: flex;
+    justify-content: space-between;
+    width: 100%;
+    padding: 2rem 1rem 12rem;
+    
+    
+    button{
+        background: var(--transaction-orange);
+        padding: 0.6rem 1.25rem;
+        border-radius: 0.3rem;
+        font-family: 'Poppins', sans-serif;
+        font-weight: 600;
+        color: var(--walter-white);
+        border: 0;
+        font-size: 1rem;
+    }
+
+    
+    
 `
