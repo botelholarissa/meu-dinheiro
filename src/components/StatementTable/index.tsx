b11ce3a4b2e49c1ee883376cb35e9da0738baac5
Danilo <danilogc4@gmail.com>
2022-08-16 19:00:20 -0300

    Update table layout - basic styles

--- src/components/StatementTable/index.tsx
+++ src/components/StatementTable/index.tsx
@@ -14,14 +14,14 @@ export function StatementTable(){
             <tbody>
                 <tr>
                     <td>Bota bonitona verde</td>
-                    <td>398,00</td>
+                    <td className="withdraw">{new Intl.NumberFormat('pt-BR',  {style: 'currency', currency: 'BRL'}).format(-398)}</td>
                     <td>Vesuário</td>
                     <td>21/07/2022</td>
                 </tr>
 
                 <tr>
                     <td>Camisa fogo nos racista</td>
-                    <td>304,00</td>
+                    <td className="deposit">{new Intl.NumberFormat('pt-BR',  {style: 'currency', currency: 'BRL'}).format(308)}</td>
                     <td>Vesuário</td>
                     <td>12/08/2022</td>
                 </tr>
