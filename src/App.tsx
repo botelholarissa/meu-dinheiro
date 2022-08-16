995f55866cf58f1a0971ca8d4417d18219c66296
Danilo <danilogc4@gmail.com>
2022-08-16 16:46:43 -0300

    Deleted unused imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,5 @@
-import React from 'react';
 import { Dashboard } from './components/Dashboard';
 import { Header } from './components/Header';
-import { StatementTable } from './components/StatementTable';
 import { GlobalStyleComponent } from './styles/global';
 
 export function App() {
