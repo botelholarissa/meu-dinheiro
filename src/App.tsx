8cf2cf6d35eb45849b12ff45f361b8e588049541
botelholarissa <conta.larissa@gmail.com>
2022-08-16 15:47:29 -0300

    create summary component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import { Header } from './components/Header';
+import { Summary } from './components/Summary';
 import { GlobalStyleComponent } from './styles/global';
 
 export function App() {
   return (
-    <><GlobalStyleComponent></GlobalStyleComponent><Header></Header><h1></h1></>
+    <><GlobalStyleComponent></GlobalStyleComponent><Header></Header><Summary></Summary><h1></h1></>
   );
 }
---
+++ src/components/Summary/index.tsx
@@ -0,0 +1,33 @@
+import incomeImg from '../../assets/income.svg';
+import outcomeImg from '../../assets/outcome.svg';
+import totalImg from '../../assets/total.svg';
+import { Container } from '../Summary/styles';
+
+export function Summary() {
+    return (
+        <Container>
+            <div>
+                <header>
+                    <img src={incomeImg} alt="Círculo verde sem preenchimento com seta verde para cima." />
+                    <h2>Entrada</h2>
+                </header>
+                <strong>R$17.400,00</strong>
+                
+            </div>
+            <div>
+                <header>
+                <img src={outcomeImg} alt="Círculo vermelho sem preenchimento com seta vermelha para baixo." />
+                <h2>Saída</h2>
+                </header>
+                <strong>R$1259,00</strong>
+            </div>
+            <div>
+                <header>
+                <img src={totalImg} alt="Cifrão branco em fundo branco." />
+                <h2>Total</h2>
+                </header>
+                <strong>R$16.141,00</strong>
+            </div>
+        </Container>
+    );
+};
