28e9430d710bd92049eed41cc58cbcd403d3d451
Danilo <danilogc4@gmail.com>
2022-08-16 15:48:10 -0300

    Add statement table to App main component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
 import React from 'react';
 import { Header } from './components/Header';
+import { StatementTable } from './components/StatementTable';
 import { GlobalStyleComponent } from './styles/global';
 
 export function App() {
   return (
-    <><GlobalStyleComponent></GlobalStyleComponent><Header></Header><h1></h1></>
+    <>
+      <GlobalStyleComponent/>
+      <Header/>
+      <StatementTable/>
+    </>
   );
 }
